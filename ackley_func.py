0fac8da019a9686d756652249c8b0e579a32bdf0
AOZMH <zhangminhao@pku.edu.cn>
2022-04-03 22:33:27 +0800

    initial commit

---
+++ ackley_func.py
@@ -0,0 +1,74 @@
+# Ackley's function, with its gradient & hessian function
+import numpy as np
+import torch
+
+
+class ackley_func:
+
+    def __init__(self):
+        self.reset()
+    
+    def reset(self, func_calls=0, g_calls=0, G_calls=0):
+        self.func_calls = func_calls
+        self.g_calls = g_calls
+        self.G_calls = G_calls
+    
+    def __call__(self, x):
+        # Ackley's function
+        # Input: x, an n-dim float vector
+        # Output: function value
+        self.func_calls += 1
+
+        v1 = -20 * torch.exp(-0.2 * (torch.mean(x ** 2) ** 0.5))
+        v2 = torch.exp(torch.mean(torch.cos(2 * np.pi * x)))
+        return v1 - v2 + 20 + np.e
+    
+    def g_ackley_func(self, x):
+        # Gradient of ackley's function
+        # Input: x, an n-dim float vector
+        # Output: g(x), an n-dim float vector
+        self.g_calls += 1
+
+        mean_sqrt = torch.mean(x ** 2) ** 0.5
+        v1 = torch.exp(-0.2 * mean_sqrt) * 4 / (len(x) * mean_sqrt) * x
+        mean_cos = torch.exp(torch.mean(torch.cos(2 * np.pi * x)))
+        v2 = mean_cos * 2 * np.pi / len(x) * torch.sin(2 * np.pi * x)
+        return v1 + v2
+
+    def G_ackley_func(self, x):
+        # Hessian of ackley's function
+        # Input: x, an n-dim float vector
+        # Output: G(x), an n*n float matrix
+        self.G_calls += 1
+
+        return torch.autograd.functional.hessian(self.__call__, x)
+    
+    def get_partial_alpha(self, xk, dk):
+        # Get 1-dim function of a for line search
+        return lambda alpha : self(xk + dk * alpha)
+
+
+def ackley_test(x):
+    a,b,c = 20.0, 0.2, 2*np.pi 
+    f  = -a*np.exp(-b*np.sqrt(np.mean(x**2)))
+    f -= np.exp(np.mean(np.cos(c*x)))
+    f += a + np.exp(1)
+    return f
+
+
+def main():
+    ackley_func_inst = ackley_func()
+    for ix in range(20):
+        a = torch.randn(32, requires_grad=True)
+        res = ackley_func_inst(a)
+        
+        delta = ackley_test(a.detach().numpy()) - res
+        assert(abs(delta) < 2e-6), delta
+        
+        res.backward()
+        delta_g = torch.norm(a.grad - ackley_func_inst.g_ackley_func(a))       
+        assert(abs(delta) < 2e-6), delta
+
+
+if __name__ == '__main__':
+    main()
---
+++ line_search.py
@@ -0,0 +1,125 @@
+
+
+from tabnanny import verbose
+
+
+def back_forth(partial_func):
+    # Back-and-forth method for initial range
+    # partial_func := func(alpha) = f_raw(alpha * dk)
+    alpha, step_size, mag_scale = 2, 1, 2
+    if partial_func(alpha) > partial_func(alpha + step_size):
+        while partial_func(alpha) > partial_func(alpha + step_size):
+            alpha += step_size
+            step_size *= mag_scale
+            #print(step_size, alpha)
+        return [alpha - step_size / mag_scale, alpha + step_size]
+    elif partial_func(alpha) > partial_func(alpha - step_size):
+        while partial_func(alpha) > partial_func(alpha - step_size):
+            alpha -= step_size
+            step_size *= mag_scale
+        return [alpha - step_size, alpha + step_size / mag_scale]
+    else:
+        return [-10, 10]
+
+
+def back_forth_test():
+    from ackley_func import ackley_func
+    import torch
+    func = ackley_func()
+    xk = torch.randn(128) / 10
+    dk = -func.g_ackley_func(xk)
+    partial_func = func.get_partial_alpha(xk, dk)
+    #print(xk)
+    res = back_forth(partial_func)
+    print(res)
+
+
+class fib_searcher:
+    # Fibbonacci search for line search
+    def __init__(self, max_fib_len=100):
+        self.fib_seq = self.get_fib_seq(max_fib_len)
+
+    def get_fib_seq(self, len_seq):
+        # Generate fibonacci sequence
+        cur_list = [1, 1]
+        for ix in range(len_seq - 2):
+            cur_list.append(cur_list[-2] + cur_list[-1])
+        return cur_list
+    
+    def fib_search(self, partial_func, init_l, init_r, n_func_calls, verbose=False):
+        # Fibonacci search on initial range [init_l, init_r]
+        assert(n_func_calls <= len(self.fib_seq))
+        a, b = init_l, init_r
+        state = 'start'
+        for ix in range(n_func_calls - 2):
+            x1 = a + self.fib_seq[n_func_calls - ix - 3] / self.fib_seq[n_func_calls - ix - 1] * (b - a)
+            x2 = a + self.fib_seq[n_func_calls - ix - 2] / self.fib_seq[n_func_calls - ix - 1] * (b - a)
+            if state == 'right':
+                assert(abs(x1 - prev_x2) < 1e-4)
+                f_x1, f_x2 = f_x2, partial_func(x2)
+            elif state == 'left':
+                assert(abs(x2 - prev_x1) < 1e-4)
+                f_x1, f_x2 = partial_func(x1), f_x1
+            else:
+                f_x1, f_x2 = partial_func(x1), partial_func(x2)
+            if verbose:
+                print('[{}] a={:.5f}, x1={:.5f}, x2={:.5f}, b={:.5f}, f(x1)={:.5f}, f(x2)={:.5f}'.format(ix+1, a, x1, x2, b, f_x1, f_x2))
+            
+            prev_x1, prev_x2 = x1, x2
+            if f_x1 >= f_x2:  # if f(x1) >= f(x2), search in (x1,b)
+                a = x1
+                x1 = x2
+                state = 'right'
+                #x2 = a + self.fib_seq[n_func_calls - ix - 3] / self.fib_seq[n_func_calls - ix - 2] * (b - a)
+            else:  # if f(x1) < f(x2), search in (a,x2)
+                b = x2
+                x2 = x1
+                state = 'left'
+                #x1 = a + self.fib_seq[n_func_calls - ix - 4] / self.fib_seq[n_func_calls - ix - 2] * (b - a)
+        
+        return (a + b) / 2
+    
+    def fib_pipeline(self, partial_func, n_func_calls):
+        # Full pipeline
+        init_l, init_r = back_forth(partial_func)
+        search_res = self.fib_search(partial_func, init_l, init_r, n_func_calls)
+        return search_res
+
+
+def fib_test():
+    from ackley_func import ackley_func
+    import torch
+    func = ackley_func()
+    xk = torch.randn(128) / 2
+    dk = -func.g_ackley_func(xk)
+    #dk = -xk
+    partial_func = func.get_partial_alpha(xk, dk)
+    fib_search_inst = fib_searcher()
+    #print(xk)
+    init_l, init_r = back_forth(partial_func)
+    #init_l, init_r = -100, 100
+    search_res = fib_search_inst.fib_search(partial_func, init_l, init_r, 50, verbose=True)
+    print(search_res)
+
+
+def gd_test():
+    from ackley_func import ackley_func
+    import torch
+    func = ackley_func()
+    xk = torch.randn(128) / 2
+    print('[0] {:.6f}'.format(func(xk)))
+    fib_search_inst = fib_searcher()
+    
+    for ix in range(100):
+        dk = -func.g_ackley_func(xk)
+        partial_func = func.get_partial_alpha(xk, dk)
+        init_l, init_r = back_forth(partial_func)
+        search_res = fib_search_inst.fib_search(partial_func, init_l, init_r, 50)
+        xk = xk + search_res * dk
+        print('[{}] {:.6f}'.format(ix, func(xk)))
+
+
+if __name__ == '__main__':
+    #back_forth_test()
+    #fib_test()
+    gd_test()
